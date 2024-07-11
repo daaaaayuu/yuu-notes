8d44446f5b87e6a73366b9e986c334f00ae1b6d4
哈嘎工程ㄙ <144036866+daaaaayuu@users.noreply.github.com>
2024-07-11 20:36:52 +0800

    Delete Spring Boot/20240711 Day12 directory

--- Spring Boot/20240711 Day12/test.java
+++
@@ -1,9 +0,0 @@
-@RestController
-public class MyController {
-    @RequestMapping("/test")
-    public ResponseEntity<String> test(){
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Hello World");
-    }
-}
-
-測試喔
